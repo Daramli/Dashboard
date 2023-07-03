608a8379d0dcbbb5220dd088c2df3588e367352c
ahmedammarsd <ahmed091999287@gmail.com>
2023-07-03 07:06:18 +0200

    test for pust

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import { FiSetting } from "react-icons/fi"
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import "./app.css"
 
 const App = () => {
